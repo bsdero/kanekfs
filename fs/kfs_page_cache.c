86ec425a5a5da607db8191d89dc94274e468fda1
Armando A. Garduno <bsdero@gmail.com>
2024-08-18 19:27:49 -0600

    fixed build issues

--- fs/kfs_page_cache.c
+++ fs/kfs_page_cache.c
@@ -12,6 +12,20 @@
 #define NANOSECS_DELAY                   100000000ul
 #define MILISECS_PER_SECOND              1000000000ul
 
+
+#ifndef timespecsub
+#define	timespecsub(tsp, usp, vsp)					\
+	do {								\
+		(vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;		\
+		(vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;	\
+		if ((vsp)->tv_nsec < 0) {				\
+			(vsp)->tv_sec--;				\
+			(vsp)->tv_nsec += 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
+
 void *kfs_pgcache_loop_thread( void *cache_p){
     pgcache_t *cache = ( pgcache_t *) cache_p;
     struct timespec remaining, request;

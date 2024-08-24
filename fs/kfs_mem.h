9965934a920220f951e159225af07844857bd1a8
bsdero <bsdero@gmail.com>
2024-08-23 21:49:18 -0600

    Superblock updated kfs_slot_cache updated

--- fs/kfs_mem.h
+++ fs/kfs_mem.h
@@ -156,8 +156,11 @@ typedef struct{
     uint64_t sb_root_super_inode; /* any inode can be the root inode */
 
     /* extents cache and cached super block */
-    void *sb_extents_cache;
-    void *sb_cache_element;
+    void *sb_page_cache;
+    void *sb_superbloch_pgcache_element;
+    void *slot_cache;
+    void *inode_cache;
+    void *graph_cache;
 
     /* super inodes capacity, used inodes, cache, and extents */
     si_table_t sb_si_table;
